ee15ba1de5caf1f5072cb451f96b12c4e12b60e7
Shridhar Bagal <shridharbagal345@gmail.com>
2025-06-15 02:50:09 +0530

    bug fix

--- backend/src/main/java/com/shridhar/prescripto/backend/EnvLoader.java
+++ backend/src/main/java/com/shridhar/prescripto/backend/EnvLoader.java
@@ -4,9 +4,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvLoader {
     public static void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
+        try{
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(entry ->
+                    System.setProperty(entry.getKey(), entry.getValue())
+            );
+        } catch (Exception e) {
+        }
     }
 }
